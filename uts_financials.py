1409a11159c9c67e54043dcec327f46d51be9bed
twosaints81 <rdossantosdpt@gmail.com>
2023-08-24 14:01:08 -0500

    Updated to include employee profiles

--- uts_financials.py
+++ uts_financials.py
@@ -1,8 +1,5 @@
-# UTS Financial Report using Streamlit
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 
@@ -10,51 +7,59 @@ def load_data(uploaded_file):
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         return df
-    
-st.image('./images/UTS_LOGO.png')
 
-# Upload CSV file for the net income, clinical pay, and profit margin data
-st.subheader('Upload your CSV data file')
-uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
 
-data = load_data(uploaded_file)
-if data is not None:
-    
-    st.subheader('Admin Dashboard')
+st.image("./images/UTS_LOGO.png")
 
-    fig = px.line(data, x='start_date', y='net_income', title='Weekly Net Income Chart')
+# Upload CSV file for net income, clinical pay, and profit margin data
+st.subheader("Upload your CSV data file")
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    data = load_data(uploaded_file)
+    fig = px.line(data, x="start_date", y="net_income", title="Weekly Net Income Chart")
     st.plotly_chart(fig)
 
-    fig2 = px.line(data, x='start_date', y='total_clinical_pay', title='Weekly Total Clinical Pay Chart')
-    st.plotly_chart(fig2)
+    fig = px.line(
+        data, x="start_date", y="total_clinical_pay", title="Weekly Clinical Pay Chart"
+    )
+    st.plotly_chart(fig)
 
-    fig3 = px.bar(data, x='start_date', y='profit_margin', title='Weekly Profit Margin Chart')
-    st.plotly_chart(fig3)
-
-    # Download button
-    st.markdown("### Download CSV file ###")
-    st.dataframe(data)
-    st.download_button("Click here to download the data as CSV file", data.to_csv(index=False), "UTS_Financial_Data.csv", "text/csv")
 
 # Upload CSV file for invoice total data
-st.subheader('Upload your Invoice CSV file')
-uploaded_invoice_file = st.file_uploader("Choose an Invoice CSV file", type='csv')
+uploaded_invoice_file = st.file_uploader("Choose an Invoice CSV file", type="csv")
 
-data_invoice = load_data(uploaded_invoice_file)
-if data_invoice is not None:
-    st.subheader('Weekly Invoice Total')
-    fig4 = px.line(data_invoice, x='start_date', y='invoice_total', title='Weekly Invoice Total Chart')
-    st.plotly_chart(fig4)
-    st.dataframe(data_invoice)
-    st.download_button("Click here to download the data as CSV file", data_invoice.to_csv(index=False), "UTS_Invoice_Data.csv", "text/csv")
+if uploaded_invoice_file is not None:
+    data_invoice = load_data(uploaded_invoice_file)
+    fig_invoice = px.line(
+        data_invoice,
+        x="start_date",
+        y="invoice_total",
+        title="Weekly Invoice Total Chart",
+    )
+    st.plotly_chart(fig_invoice)
 
-st.write('---')
-st.subheader('I plan to incorporate Batchgeo Here')
+# Upload Employees Profile CSV file
+uploaded_employee_profile = st.file_uploader(
+    "Choose the Employee Profile file", type="csv"
+)
 
-# Upload CSV file for map data
-st.subheader('Upload your Map CSV file')
-uploaded_map_file = st.file_uploader("Choose a Map CSV file", type='csv')
+if uploaded_employee_profile is not None:
+    data_employee_profile = load_data(uploaded_employee_profile)
+    st.dataframe(data_employee_profile)
 
-data_map = load_data(uploaded_map_file)
-if data_map is not None:
-    st.map(data_map)
+    # Filter the data based on your needs
+    filtered_data = data_employee_profile
+
+    # Interactive Details
+    st.title("Employee Details")
+    selected_employee = st.selectbox(
+        "Select Employee", options=filtered_data["Provider_Last"].unique().tolist()
+    )
+    employee_details = filtered_data[
+        filtered_data["Provider_Last"] == selected_employee
+    ]
+    st.bar_chart(employee_details[["Total_Compensation"]])
+
+    # You can add more specific visualizations based on the data and needs
+    st.area_chart(employee_details[["Total_Compensation"]])
